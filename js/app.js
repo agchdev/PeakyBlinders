656a7f53c7866fbb767112ced397d616f35be25e
unknown <alex.chinchi02@gmail.com>
2025-02-12 11:33:28 +0100

    cambios

--- js/app.js
+++ js/app.js
@@ -10,34 +10,59 @@ document.addEventListener("DOMContentLoaded", () => {
     const actRiv = document.getElementById("actRiv");
     const womenPow = document.getElementById("womenPow");
     const michaelShelbi = document.getElementById("michaelShelbi");
-    const liHeader = document.querySelectorAll(".cursorHover")
+    const putos = document.getElementById("putos");
+    const disparos = document.getElementById("disparos");
+    const liHeader = document.querySelectorAll(".cursorHover");
 
+    const textPutos = ["P****","*U***","**T**","***O*","****S"]
+
+    animatePutos();
+    
     // Al mover el mouse, actualizamos la posición del cursor
     window.addEventListener("mousemove", (e) => {
         // Mostramos el cursor (por si estaba oculto)
         cursor.style.opacity = 1;
-
+        
         // Posicionamos el div en la posición del mouse
         cursor.style.left = `${e.clientX}px`;
         cursor.style.top = `${e.clientY}px`;
     });
-
+    
     // Si el mouse sale de la ventana, ocultamos el cursor
     window.addEventListener("mouseleave", () => {
         cursor.style.opacity = 0;
     });
-
+    
     cerrarHeader.addEventListener("click", () => {
         nav.classList.add("animate-closeHeader")
         nav.classList.remove("animate-openHeader")
     })
-
+    
     btnHeader.addEventListener("click", () => {
         nav.classList.add("animate-openHeader")
         nav.classList.remove("animate-closeHeader")
         nav.classList.remove("hidden")
     })
     
+    disparos.addEventListener("click", (e) => {
+        let bullet = document.createElement("img");
+        bullet.src = "img/bullet.png";
+        bullet.style.position = "absolute";
+        bullet.style.width = "5%";
+
+        console.log(e.clientX)
+        let x = e.clientX-47;
+        let y = e.clientY-43;
+
+        bullet.style.top = `${y}px`;
+        bullet.style.left = `${x}px`;
+
+        disparos.append(bullet);
+
+        setTimeout(() => {
+            bullet.remove();
+        }, 2000);
+    })
     
     
     actRiv.addEventListener("mouseenter", () => {
@@ -57,7 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
         document.querySelector("#womenPow > div:first-child").style.transform = "translate(0px, 0%)"
         document.querySelector("#womenPow > div:nth-child(2)").style.transform = "translate(0px, 0%)"
     })
-
+    
     michaelShelbi.addEventListener("mouseenter", () => {
         document.querySelector("#michaelShelbi > div:first-child").style.transform = "translate(0px, -100%)"
         document.querySelector("#michaelShelbi > div:nth-child(2)").style.transform = "translate(0%, 100%)"
@@ -75,11 +100,20 @@ document.addEventListener("DOMContentLoaded", () => {
     encogeAgranda(cerrarHeader, false, "Cerrar")
     encogeAgranda(netflix, true, "Click")
     encogeAgranda(videoYT, true, "Play")
-
+    
     liHeader.forEach(el => {
         encogeAgranda(el, true, "")
     })
 
+    let pos = 0;
+    function animatePutos(){
+        setInterval(() => {
+            putos.innerText = `${textPutos[pos]}`;
+            pos++;
+            if(pos == 5) pos = 0
+        }, 800);
+    }
+    
     function encogeAgranda(el, muestra, text){
         el.addEventListener("mouseenter", () => {
             agrandarMouse(el, muestra, text)
@@ -88,7 +122,7 @@ document.addEventListener("DOMContentLoaded", () => {
             encogerMouse(el, muestra)
         })
     }
-
+    
     // Agrandar mouse cuando me interese
     function agrandarMouse(el, muestra, texto){
         cursor.classList.add("animate-scaleCircle")
@@ -99,7 +133,7 @@ document.addEventListener("DOMContentLoaded", () => {
         cursor.style.color = "white";
         if(texto != "") cursor.innerText = `${texto}`
     }
-
+    
     // Encoger mouse cuando me interese
     function encogerMouse(el, muestra){
         cursor.classList.remove("animate-scaleCircle")
